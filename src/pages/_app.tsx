35c703c24e359e2b413307746e6693134a18461b
Fabrício M. Damasceno <fabricio.damasceno@outlook.com.br>
2024-02-02 15:50:13 -0300

    Adicionando e configurando Tailwind manualmente.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import type { AppProps } from "next/app";
+import { Poppins } from 'next/font/google';
+import '../styles/globals.css';
+
+const poppins = Poppins({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+  variable: '--font-poppins'
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={`${poppins.variable} font-sans`}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
 
